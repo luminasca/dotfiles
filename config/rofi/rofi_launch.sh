9322b45c015fccdf9d1408086db3abe8e8432b95
luminasca <guiganyahho@gmail.com>
2021-11-07 21:10:31 +0900

    add setting file

---
+++ config/rofi/rofi_launch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# rofi_launch.sh / JennyM 2016 malkalech.com
+
+alpha="cc"   # opacity (00〜FF)
+
+options=(
+  -modi            "drun,system:~/.config/rofi/rofi_system.sh"
+  -show            "drun"
+  -font            "MotoyaLMaru 28"
+  -width           "90"
+  -padding         "80"
+  -lines           "8"
+  -fixed-num-lines
+  -hide-scrollbar
+  -sidebar-mode
+
+  ##  key bindings  ##
+  -kb-cancel        "Escape,Control+g,Control+bracketleft,Control+c"
+  -kb-mode-next     "Shift+Right,Control+i,Control+Tab"
+  -kb-mode-previous "Shift+Left,Control+Shift+i"
+
+  # colorscheme
+  -theme "/usr/share/rofi/themes/Arc-Dark.rasi"
+
+)
+
+rofi "$@" "${options[@]}"
