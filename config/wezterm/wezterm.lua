d497d965452bb59f01d688997f34e4c9132f2265
luminasca <shonto.shonte@gmail.com>
2023-04-12 18:59:53 +0900

    add pane keybinds

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,10 +1,21 @@
 local wezterm = require 'wezterm';
 
 return {
+  keys = {
+    {
+      key = 'v',
+      mods = 'CTRL|ALT',
+      action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain"},
+    },
+    {
+      key = 'h',
+      mods = 'CTRL|ALT',
+      action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+    },
+  },
   font = wezterm.font("HackGen35 Console NF", {weight = "Bold"}), -- 自分の好きなフォントいれる
   use_ime = true, -- wezは日本人じゃないのでこれがないとIME動かない
   font_size = 13.0,
-  color_scheme = "iceberg-dark", -- 自分の好きなテーマ探す https://wezfurlong.org/wezterm/colorschemes/index.html
-  hide_tab_bar_if_only_one_tab = true,
+  color_scheme = "iceberg-dark",
   adjust_window_size_when_changing_font_size = false,
 }
