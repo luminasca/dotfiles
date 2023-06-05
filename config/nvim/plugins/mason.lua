0c1ed9dd538cbe31f1dff3e7e3a3ffa8cedf2b8b
luminasca <shonto.shonte@gmail.com>
2023-06-06 00:06:00 +0900

    change LSP plugins

---
+++ config/nvim/plugins/mason.lua
@@ -0,0 +1,10 @@
+local mason = require('mason')
+ mason.setup({
+   ui = {
+     icons = {
+       package_installed = "✓",
+       package_pending = "➜",
+       package_uninstalled = "✗"
+     }
+   }
+ })
