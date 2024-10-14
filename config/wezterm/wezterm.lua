dcff4fc48d1086409c86df18856de035f368f06b
luminasca <con@localhost.localdomain>
2024-10-14 13:41:02 +0900

    add pane border color

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,12 +5,12 @@ return {
     {
       key = 'v',
       mods = 'CTRL|SUPER',
-      action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain"},
+      action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain"},
     },
     {
       key = 'h',
       mods = 'CTRL|SUPER',
-      action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+      action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
     },
   },
   font = wezterm.font("HackGen35 Console NF", {weight = "Regular"}), -- 自分の好きなフォントいれる
@@ -18,6 +18,10 @@ return {
   font_size = 13.0,
   color_scheme = "nord",
   enable_wayland = false,
-  window_background_opacity = 0.5,
+  window_background_opacity = 0.8,
   adjust_window_size_when_changing_font_size = false,
+  colors = {
+      split = "#32a887",
+
+  },
 }
